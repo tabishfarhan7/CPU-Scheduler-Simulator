4d9ff6407a97c4625f0fceed82da75e0ff54c567
MD TABISH FARHAN <150028841+tabishfarhan7@users.noreply.github.com>
2025-03-20 14:14:26 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import tkinter as tk
+from ttkbootstrap import Style
+from gui.app_ui import create_ui
 
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("CPU Scheduler")
+    style = Style(theme="solar")
+
+    root.geometry("900x600")
+    create_ui(root)
+    root.mainloop()
